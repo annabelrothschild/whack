9d27ec100b1579f8b46853339ee7d9d8e7d59f46
Annabel Rothschild <arothsch@wellesley.edu>
2017-11-11 11:27:52 -0500

    collect search terms

---
+++ getInput.js
@@ -0,0 +1,6 @@
+ function collectSearches(){
+    var q1 =document.getElementById('input1').value;
+    var q2 =document.getElementById('input2').value;
+    document.getElementById("img1").innerHTML = q1;
+    document.getElementById("img2").innerHTML = q2;
+ }
